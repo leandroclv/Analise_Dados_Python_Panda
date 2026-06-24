7314f892f67de9fe40f3e83759fd65db344998e9
Leandro Costa Lima Verde <leandroclv@gmail.com>
2026-06-23 21:31:12 -0300

    update

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px 
 from dataset import df
+from utils import format_number
+from graficos import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado, grafico_rec_categoria, grafico_rec_vendedores, grafico_vendas_vendedores
 
 
 st.set_page_config(layout='wide')
@@ -10,5 +12,20 @@ aba1,aba2, aba3 = st.tabs(['Dataset', 'Receita', 'Vendedores'])
 
 with aba1:
     st.dataframe(df)
-
-#pip install --upgrade plotly
+    
+with aba2:
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita Total', format_number(df['Preço'].sum(), 'R$' ))
+        st.plotly_chart(grafico_map_estado, use_container_width=True)
+        st.plotly_chart(grafico_rec_estado, use_container_width=True)
+    with coluna2:
+        st.metric('Quantidade de Vendas', format_number(df.shape[0]))
+        st.plotly_chart(grafico_rec_mensal, use_container_width=True)
+        st.plotly_chart(grafico_rec_categoria, use_container_width=True)
+with aba3:
+    coluna1,coluna2 = st.columns(2)
+    with coluna1:
+        st.plotly_chart(grafico_rec_vendedores, use_container_width=True)
+    with coluna2:    
+        st.plotly_chart(grafico_vendas_vendedores, use_container_width=True)
---
+++ streamlit/utils.py
@@ -0,0 +1,25 @@
+from dataset import df
+import pandas as pd
+
+
+def format_number(value, prefix = ''):
+    for unit in ['', 'mil']:
+        if value < 1000:
+            return f'{prefix} {value:.2f} {unit}'
+        value /=1000
+    return f'{prefix} {value:.2f} milhões'
+
+#DataFrame Receita por Estado
+df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
+df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(df_rec_estado, left_on = 'Local da compra', right_index = True).sort_values('Preço', ascending=False)
+
+#DataFrame Receita Mensal
+df_rec_mensal = df.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].sum().reset_index()
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name()
+
+#DataFrame Receita por Categoria
+df_rec_categoria = df.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
+
+#DataFrame Vendedor
+df_vendedores = pd.DataFrame(df.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
